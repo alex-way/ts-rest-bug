9f455aff1851d707cd980d03fd1a9691ed2ce41d
Alex Way <alexwa@softcat.com>
2024-04-19 09:23:37 +0100

    Update deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,14 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import type { AppProps } from 'next/app';
-import React from 'react';
-import '../styles/globals.css';
-
-const ReactQueryDevtoolsProduction = React.lazy(() =>
-  import('@tanstack/react-query-devtools/build/lib/index.prod.js').then(
-    (d) => ({
-      default: d.ReactQueryDevtools,
-    })
-  )
-);
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import type { AppProps } from "next/app";
+import React from "react";
+import "../styles/globals.css";
 
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [showDevtools, setShowDevtools] = React.useState(false);
-
-  React.useEffect(() => {
-    // @ts-ignore
-    window.toggleDevtools = () => setShowDevtools((old) => !old);
-  }, []);
-  return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen />
-      {showDevtools && (
-        <React.Suspense fallback={null}>
-          <ReactQueryDevtoolsProduction />
-        </React.Suspense>
-      )}
-    </QueryClientProvider>
-  );
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Component {...pageProps} />
+		</QueryClientProvider>
+	);
 }
